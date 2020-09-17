3a2f3af1299c20a1977cf0a881f51ad6f1ee84f3
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-17 11:13:32 -0700

    fail second test Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com> Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com> Co-authored-by: JohnNilsOlson <johnnils@gmail.com>

--- src/__tests___/reducers/post-list-reducer.test.js
+++ src/__tests___/reducers/post-list-reducer.test.js
@@ -2,7 +2,36 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+
+  let action;
+  const postData = {
+    postTitle: "Redux rox!",
+    postAuthor: "User2000",
+    postBody: "i love programmming",
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new post data to masterPostlist', () => {
+    const { postTitle, postAuthor, postBody, id } = postData;
+    action = {
+      type: 'ADD_TICKET',
+      postTitle: postTitle,
+      postAuthor: postAuthor,
+      postBody: postBody,
+      id: id
+  };
+
+  expect(postListReducer({}, action)).toEqual({
+    [id] : {
+      postTitle: postTitle,
+      postAuthor: postAuthor,
+      postBody: postBody,
+      id: id
+    }
+  });
+});
 });
