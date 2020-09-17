4c66a4c68df341c890992f4520e461cfa21a2494
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-17 11:59:11 -0700

    passing 3rd test Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com> Co-authored-by: JohnNilsOlson <johnnils@gmail.com>

--- src/__tests___/reducers/post-list-reducer.test.js
+++ src/__tests___/reducers/post-list-reducer.test.js
@@ -1,14 +1,23 @@
-
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
 
-  let action;
-  const postData = {
-    postTitle: "Redux rox!",
+  let action;  
+  const currentState = {
+    1: {postTitle: "Redux rox!",
     postAuthor: "User2000",
     postBody: "i love programmming",
-    postId: 1
+    postStamp: "9-10-2005",
+    PostPeek: "kjsdlkfj",
+    PostVote: 0,
+    postId: 1},
+    2: {postTitle: "Redux is rough!",
+    postAuthor: "User2002",
+    postBody: "i hate programmming",
+    postStamp: "9-10-2003",
+    PostPeek: "kjsdddddlkfj",
+    PostVote: 0,
+    postId: 2}
   };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -16,22 +25,44 @@ describe('postListReducer', () => {
   });
 
   test('Should successfully add new post data to masterlist', () => {
-    const { postTitle, postAuthor, postBody, postId } = postData;
+    const { postTitle, postAuthor, postBody, postStamp, postVote, postPeek, postId } = currentState;
     action = {
       type: 'ADD_POST',
       postTitle: postTitle,
       postAuthor: postAuthor,
       postBody: postBody,
+      postStamp: postStamp,
+      postPeek: postPeek,
+      postVote: postVote,
       postId: postId
-  };
-
-  expect(postListReducer({}, action)).toEqual({
-    [postId] : {
-      postTitle: postTitle,
-      postAuthor: postAuthor,
-      postBody: postBody,
-      postId: postId
-    }
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [postId] : {
+        postTitle: postTitle,
+        postAuthor: postAuthor,
+        postBody: postBody,
+        postStamp: postStamp,
+        postPeek: postPeek,
+        postVote: postVote,
+        postId: postId
+      }
+    });
   });
-});
+
+  test('Should delete a post', () => {
+  action = {
+    type: 'DELETE_POST',
+    postId: 1
+  };
+    expect(postListReducer(currentState, action)).toEqual({
+      2: {postTitle: "Redux is rough!",
+      postAuthor: "User2002",
+      postBody: "i hate programmming",
+      postStamp: "9-10-2003",
+      PostPeek: "kjsdddddlkfj",
+      PostVote: 0,
+      postId: 2 }
+    });
+  });
+
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,16 +1,23 @@
 export default (state = {}, action) => {
-  const { postTitle, postAuthor, postBody, postId } = action;
+  const { postTitle, postAuthor, postBody, postPeek, postVote, postStamp, postId } = action;
   switch (action.type) {
-    case 'ADD_POST':
-      return Object.assign({}, state, {
-        [postId]: {
-          postTitle: postTitle,
-          postAuthor: postAuthor,
-          postBody: postBody,
-          postId: postId
-        }
-      });
-      default:
-        return state;
+  case 'ADD_POST':
+    return Object.assign({}, state, {
+      [postId]: {
+        postTitle: postTitle,
+        postAuthor: postAuthor,
+        postBody: postBody,
+        postPeek: postPeek,
+        postVote: postVote,
+        postStamp: postStamp,
+        postId: postId
+      }
+    });
+  case 'DELETE_POST':
+    const newState = { ...state };
+    delete newState[postId];
+    return newState;
+  default:
+    return state;
     }
 };
